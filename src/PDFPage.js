08b3972b37a83b0df7664dbf368d7c5e3a823908
Kotaro7750 <7750koutarou@gmail.com>
2021-11-23 12:06:03 +0900

    実機で表示すると解像度が低くてぼやけていたのを修正
    デバイスピクセル比が1じゃなかったせいでページが引き伸ばされていたのが原因

--- src/PDFPage.js
+++ src/PDFPage.js
@@ -66,32 +66,48 @@ export class PDFPage extends React.Component {
 
       const canvas = this.canvasRef.current;
       const ctx = canvas.getContext('2d');
-      const cardBodyContainer = document.getElementById('page-container-body-' + String(this.props.pageNumber));
 
+      const cardBodyContainer = document.getElementById('page-container-body-' + String(this.props.pageNumber));
       const containerComputedStyle = window.getComputedStyle(cardBodyContainer);
 
       const containerWidth = cardBodyContainer.clientWidth - parseFloat(containerComputedStyle.paddingLeft) - parseFloat(containerComputedStyle.paddingRight);
       const browserViewportHeight = window.innerHeight;
 
-      const scaleAdjustHeight = browserViewportHeight / page.getViewport({ scale: 1.0 }).height;
-      const scaleAdjustWidth = containerWidth / page.getViewport({ scale: 1.0 }).width;
+      // 親要素の幅とブラウザビューポートの高さに合わせた際のスケールを計算
+      const originalViewport = page.getViewport({ scale: 1 });
+      const scaleAdjustViewportHeight = browserViewportHeight / originalViewport.height;
+      const scaleAdjustParentWidth = containerWidth / originalViewport.width;
 
-      let scale;
-      // 幅のみに合わせなければいけないときを除き，幅・高さの制約を両方満たすようにスケールを調整する
+      // 幅のみに合わせなければいけないときを除き，幅・高さの制約を両方満たすようにする
+      let scaleForCSSPixel;
       if (this.props.adjustWidth) {
-        scale = scaleAdjustWidth;
+        scaleForCSSPixel = scaleAdjustParentWidth;
       } else {
-        scale = Math.min(scaleAdjustHeight, scaleAdjustWidth);
+        scaleForCSSPixel = Math.min(scaleAdjustViewportHeight, scaleAdjustParentWidth);
       }
 
-      const viewport = page.getViewport({ scale: scale });
+      // DOM要素としてのcanvasのサイズは画面に合わせてサイズを変える必要がある
+      // ここでのピクセルの値はCSSピクセル値
+      const cssViewport = page.getViewport({ scale: scaleForCSSPixel });
+      canvas.style.width = cssViewport.width + 'px';
+      canvas.style.height = cssViewport.height + 'px';
 
-      canvas.width = viewport.width;
-      canvas.height = viewport.height;
+      const devicePixelRatio = window.devicePixelRatio;
+      // 描画されるページ自体の解像度は極力下げない
+      // ここで指定しているのはcanvasの描画バッファのサイズ
+      // ここで指定した解像度が実際に描画される際の解像度となる
+      canvas.width = cssViewport.width * devicePixelRatio;
+      canvas.height = cssViewport.height * devicePixelRatio;
+
+      // 実際のレンダーのやり方としては2種類ある
+      // * 描画バッファとオリジナルページのサイズの比をスケールとするビューポート(deviceViewport)をレンダリングする方法
+      // * canvasにデバイスピクセル比分のスケールをかけて(ctx.scale(devicePixelRatio,devicePixelRatio))，cssピクセルスケールのビューポート(cssViewport)をレンダリングする方法
+      // 少なくとも開発時点では差が分からなかったので前者にした
+      const deviceViewport = page.getViewport({ scale: canvas.height / originalViewport.height });
 
       let renderContext = {
         canvasContext: ctx,
-        viewport: viewport
+        viewport: deviceViewport
       };
 
       CachedPDFjsWrapper.renderPage(renderContext, this.props.filename, page);
