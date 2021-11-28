009ad5a6d1bcfc6f7af62a0c4896009a2fffcbf2
Kotaro7750 <7750koutarou@gmail.com>
2021-11-28 21:47:02 +0900

    作品画面に作品情報を微妙に載せた

--- src/Work.js
+++ src/Work.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import { PDFRangeViewer } from './PDFRangeViewer.js';
 
 
-export function Work(props) {
+export function Work() {
   // Link経由で作品情報を受け取っているが介さない場合にはどうなるのか？
   const location = useLocation();
   let workInformation = location.state.work;
 
   return (
     <div>
+      <div>
+        <Link to={`/works?artist=${workInformation.artist}`} style={{ textDecoration: 'none' }} className='badge bg-primary rounded-pill text-wrap' >
+          {workInformation.artist}
+        </Link>
+        <Link to={`/works/?artist=${workInformation.artist}&book_title=${workInformation.book_title}`} style={{ textDecoration: 'none' }} className='badge bg-primary rounded-pill text-wrap' >
+          {workInformation.book_title}
+        </Link>
+      </div>
       <PDFRangeViewer workInformation={workInformation} />
     </div>
   );
