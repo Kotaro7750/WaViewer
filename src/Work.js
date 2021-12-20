c6f99066cc26a54b54d95ff5db339a236accc0c5
Kotaro7750 <7750koutarou@gmail.com>
2021-12-20 09:56:30 +0900

    シリーズ物作品の前後を表示できるようにした

--- src/Work.js
+++ src/Work.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
+import { useWorkInformationList } from './FileUtil.js';
 import { PDFRangeViewer } from './PDFRangeViewer.js';
+import { Thumbnail } from './Thumbnail.js';
 
 
 export function Work() {
   // Link経由で作品情報を受け取っているが介さない場合にはどうなるのか？
   const location = useLocation();
   let workInformation = location.state.work;
+
+  const workInformationList = useWorkInformationList();
+  const prevWorkInformation = workInformationList[workInformation.prevWorkId];
+  const nextWorkInformation = workInformationList[workInformation.nextWorkId];
+
+  const prevThumbnail = prevWorkInformation && <Thumbnail workInformation={prevWorkInformation} />
+  const nextThumbnail = nextWorkInformation && <Thumbnail workInformation={nextWorkInformation} />
 
   return (
     <div>
@@ -18,7 +27,18 @@ export function Work() {
           {workInformation.book_title}
         </Link>
       </div>
+
       <PDFRangeViewer workInformation={workInformation} />
+
+      <div className='row'>
+        <div className='col-xl-2 col-lg-4 col-md-6'>
+          {prevThumbnail}
+        </div>
+
+        <div className='col-xl-2 offset-xl-8 col-lg-4 offset-lg-4 col-md-6'>
+          {nextThumbnail}
+        </div>
+      </div>
     </div>
   );
 }
